ce7d8dd73cbb765f137a650230d307981d446ac7
marco monticciolo <marcomonticciolo@marcos-MBP.attlocal.net>
2022-10-27 11:23:04 -0400

    solved lab

--- js/index.js
+++ js/index.js
@@ -1,42 +1,87 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  let priceElement = product.querySelector('.price span');
+  let quantityElement = product.querySelector('.quantity input')
+ 
+  let price = priceElement.innerHTML
+  let quantity = quantityElement.value 
 
-  //... your code goes here
+  let priceTotal = price*quantity
+  let subTotalValue = product.querySelector('.subtotal span');
+
+  subTotalValue.innerHTML = priceTotal
+
+
+
+  console.log('Calculating subtotal, yey!', priceTotal );
+
+  return priceTotal
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
-  // ITERATION 2
-  //... your code goes here
 
-  // ITERATION 3
-  //... your code goes here
+  const allProducts = document.querySelectorAll('.product');
+
+  let total = 0;
+
+  for (let i = 0; i < allProducts.length; i++){
+    total = total + updateSubtotal(allProducts[i])
+
+  
+  }
+
+let productValues = document.querySelector("#total-value span");
+
+return productValues.innerHTML = total
 }
 
-// ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+
+ target.parentElement.parentElement.remove()
+
+  calculateAll
+
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+
+  let newProduct = document.querySelector('.product').cloneNode(true)
+console.log(newProduct);
+
+  document.querySelector('tbody').appendChild(newProduct)
+
+  let productName = document.querySelector('#product-name').value
+  let productPrice = document.querySelector('#product-price').value
+
+  newProduct.querySelector('.price span').innerHTML = productPrice;
+  newProduct.querySelector('.name span').innerHTML = productName;
+  
+ let removeButton = newProduct.querySelector('.btn.btn-remove')
+
+ removeButton.addEventListener('click',removeProduct )
+
+ 
+
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  let removeButton = document.querySelectorAll('.btn.btn-remove')
+
+  for (let i = 0; i < removeButton.length; i++){
+    removeButton[i].addEventListener('click', removeProduct)
+  }
+  console.log('whatsup');
+
+  //
+  let createButton = document.querySelector('#create')
+  createButton.addEventListener('click',createProduct)
 });
